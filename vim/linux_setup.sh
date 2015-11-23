3fb2ef6b9c906cecfbd1783e93529fd4a58ed33c
xenophy <kotsutsumi@xenophy.com>
2015-11-24 02:22:20 +0900

    update

--- vim/linux_setup.sh
+++ vim/linux_setup.sh
@@ -1,6 +1,2 @@
-cd ~/
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
-./install.sh
-rm install.sh
 git clone https://github.com/xenophy/kotsutsumi.git vimenv
 cp vimenv/vim/.vimrc ~/
